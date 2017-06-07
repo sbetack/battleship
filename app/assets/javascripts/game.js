0e3e0beb63e2bee415cd86e9caa6b33b6346f645
Sarah Betack <msbetack@gmail.com>
2017-06-07 12:24:02 -0500

    create models

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function() {
   console.log("hey");
-  $(document).on("click", ".overlay-box", function(){
-    $(this).css("background-color", "yellow")
+  $(document).on("hover", ".overlay-box", function(){
+    // $(this).css("background-color", "yellow")
   });
 }) 
