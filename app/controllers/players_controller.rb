b0ea38ae22d5d3aafab552efc81a834a7c27cb15
Sarah Betack <msbetack@gmail.com>
2017-06-14 17:18:25 -0500

    button working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,5 +2,30 @@ class PlayersController < ApplicationController
 
   def new 
     @games = Game.all
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      if player_params[:game_id]
+        game_id = player_params[:game_id]
+        @player.games << Game.find(game_id)
+        redirect_to "/deploy/#{game_id}"
+      else
+        game = Game.new()
+        @player.games << game
+        redirect_to "/deploy/#{game.id}"
+      end
+    else 
+      @errors = @player.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def player_params
+      params.require(:player).permit(:username, :game_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'players#new'
-  get '/game' => 'games#show'
+  get '/', :to => 'players#new'
+  get '/game/:id', :to => 'games#show'
+  resources :players
+  get '/deploy/:game_id', :to => 'games#deploy_fleet'
 end
