78b4867408d1187c9ef7c87ecc66965ed1fdd1a9
Sarah Betack <msbetack@gmail.com>
2017-06-21 12:58:44 -0500

    fix game start

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,14 +6,14 @@ class PlayersController < ApplicationController
   end
 
   def create
-    @player = Player.new(player_params)
+    @player = Player.new(username: player_params[:username])
     if @player.save
       if player_params[:game_id]
         game_id = player_params[:game_id]
         @player.games << Game.find(game_id)
         redirect_to "/deploy/#{game_id}"
       else
-        game = Game.new()
+        game = Game.create()
         @player.games << game
         redirect_to "/deploy/#{game.id}"
       end
